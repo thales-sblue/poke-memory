2b7c181897e12d31197497295810839fce2c8d1a
thales.santos <thales.santos@coopertec.com.br>
2025-07-24 16:49:50 -0300

    implementacao icones da grid

--- src/components/GridItem/index.tsx
+++ src/components/GridItem/index.tsx
@@ -10,9 +10,12 @@ type Props = {
 
 export const GridItem = ({ item, onClick }: Props) => {
   return (
-    <C.Container onClick={onClick}>
+    <C.Container
+      showBackground={item.permanentShown || item.shown}
+      onClick={onClick}
+    >
       {item.permanentShown === false && item.shown === false &&
-        <C.Icon src={b7Svg} alt="" />
+        <C.Icon src={b7Svg} alt="" opacity={.1} />
       }
       {(item.permanentShown || item.shown) && item.item !== null &&
         <C.Icon src={items[item.item].icon} alt="" />
--- src/components/GridItem/styles.ts
+++ src/components/GridItem/styles.ts
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-	background-color: blue;
+type ContainerProps = {
+	showBackground: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+	background-color: ${props => props.showBackground ? '#1550FF' : '#E2E3E3'};
+	height: 100px;
+	border-radius: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
 `;
 
-export const Icon = styled.img``;
+type IconProps = {
+	opacity?: number;
+}
+
+export const Icon = styled.img<IconProps>`
+	width: 40px;
+	height: 40px;
+	opacity: ${props => props.opacity ?? 1};
+`;
